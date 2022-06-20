c99f93ce5545c7c153c2ffae8bd5ff685da51fcf
Fran <franferreraszl@gmail.com>
2022-06-20 15:03:42 +0200

    Add anotación JsonIngonere a columna

---
+++ Desarrollo del proyecto/TrabajoFinal/src/main/resources/data.sql
@@ -0,0 +1,28 @@
+INSERT INTO usuarios (activo,addreess_line,apellidos,dni,email,nombre,password,phone,`role`,usuario) VALUES
+	 (1,'C/ Cardenal Spínola','Ferreras ','21153854H','franferreraszl@gmail.com','Francisco','joseluis','626740787','user','Fran'),
+	 (1,'C/ Cardenal Spínola','Rodriguez Velasco','12312419L','daniela7.drv@gmail.com','Daniela Alejandra','12345','626740787','user','Daniela'),
+	 (1,'Caminante fachero','Lorel Herrante','21153854H','fdafd@gmail.com','Carlos','12234545','234242342','user','Carlos'),
+	 (1,'C/ Cardenal Spínola','Ferrrs Fariñas','21153856L','francissco@gmail.com','Francisco2','joseluis','626740787','user','Fran2'),
+	 (1,'C/ Cardenal Spínola','Ferreras Fariñas','21153855J','lola@gmail.com','Lola','','626740787','ROLE_ADMIN','Lola'),
+	 (1,'C/Garrido Gutierrez n4','Valverde Ponce','12345678N','employee2@gmail.com','Hernesto','12234545','234242342','user','Hernesto'),
+	 (1,'C/ Cardenal Spínola','Ferreras Fariñas','21153852J','admin@gmail.com','Francisco','$2a$15$4gMMahiyPKvHcwe/YwRd.eUFybe4ZBIGqofdjBN6MvMGkjkeUZK8C','626740787','ROLE_ADMIN','admin'),
+	 (1,'C/ Cardenal Spínola','Ferreras Fariñas','12345678G','joseluis@gmail.com','Jose Luis','$2a$15$hTsCctGwPufVk2pVl2mBfO0ouDnrH1AwXcQGQua7q3DuJJCWfm2Dy','626740787','ROLE_USER','Jose Luis'),
+	 (1,'C/Cardenal Espinola nº6','Fariña Sanchez','21153823W','mirthamagg@gmail.com','Mirtha Lizeth','$2a$15$ZrBpURk2hu2tyLWLABD1cu.T7o5yGQq7J0j12gEuWs7bOn9j3HDuu','626740787','user','mirtha'),
+	 (1,'C/Los pajaritos','Valdes Alcazar','12345678Q','fernando@gmail.com','Fernando L','$2a$15$mvbdgfSQhDGcNlrfzUFuYOfgdkH3.bPCdKQQpSsB1/ll6hLcaT1lO','123456789','ROLE_EMPLOYEE','Fernando');
+INSERT INTO usuarios (activo,addreess_line,apellidos,dni,email,nombre,password,phone,`role`,usuario) VALUES
+	 (1,'C/Los pajaritos','Valdes Sanchez','12345678T','victor@gmail.com','Victor','$2a$15$0QET89LLKTOtKILGM9D8C.ZVRDI7aVAzXHAQlv76i/Nk0D229Z.EC','123456789','ROLE_EMPLOYEE','Victor');
+	 
+ INSERT INTO customer (id) VALUES
+ (1),
+ (2),
+ (4),
+ (5),
+ (7),
+ (8),
+ (9);
+ 
+ INSERT INTO GeneralMotors2.comercial (job_title,id,id_office,sueldo) VALUES
+	 ('employee',3,NULL,0.0),
+	 ('employee',6,NULL,0.0),
+	 ('COMERCIAL',13,NULL,1200.0),
+	 ('COMERCIAL',14,NULL,1200.0);
